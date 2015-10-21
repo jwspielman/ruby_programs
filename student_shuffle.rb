3a3cb957f8459bacec5a92eef078b27e1aec664d
Your Actual Name <Your Actual Email>
2015-10-21 10:32:13 -0400

    paired up students

--- student_shuffle.rb
+++ student_shuffle.rb
@@ -1,4 +1,12 @@
 students = %w(Aaron Janet Jame Jude Jackson Jessie Lucus Michael Jeff) # %w allows you to make an array wtih strains without commas or quotes.
 
-puts students
-puts "#{students}"
+students.shuffle!
+
+count = 0
+
+until count == (students.length-1) #makes it so all names are used
+
+puts "#{students[count]} & #{students[count+1]}" #puts the names instead of the count}
+	
+	count+=2
+end
